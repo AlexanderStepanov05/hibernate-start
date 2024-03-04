9b514d10882c307897c51d7657e8af819baba64b
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2024-03-05 02:09:39 +0300

    logging

--- src/main/java/org/example/HibernateRunner.java
+++ src/main/java/org/example/HibernateRunner.java
@@ -1,37 +1,44 @@
 package org.example;
 
-import org.example.converter.BirthdayConverter;
+import lombok.extern.slf4j.Slf4j;
 import org.example.entity.User;
 import org.example.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@Slf4j
 public class HibernateRunner {
+
+//    private static final Logger log = LoggerFactory.getLogger(HibernateRunner.class);
+
     public static void main(String[] args) {
         User user = User.builder()          // transient to s1 and s2
-                .username("ivan@gmail.com")
+                .username("ivan1@gmail.com")
                 .firstname("Ivan")
                 .lastname("Ivanov")
                 .build();
 
-        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
-            try (Session session1 = sessionFactory.openSession()) {
-                session1.beginTransaction();
+        log.info("User entity is in transient state, object: {}", user);
 
-                session1.saveOrUpdate(user); // persistent to s1, transient to s2
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
+            Session session1 = sessionFactory.openSession();
+            try (session1) {
+                Transaction transaction = session1.beginTransaction();
+                log.trace("Transaction is created: {}", transaction);
+
+                session1.persist(user); // persistent to s1, transient to s2
+
+                log.trace("User is in persistent state: {}, session {}", user, session1);
 
                 session1.getTransaction().commit();
             }
-
-            try (Session session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-//                session2.remove(user);
-//                refresh/merge
-
-                session2.getTransaction().commit();
-            }
+            log.warn("User is in detached state: {}, session is closed {}", user, session1);
+        } catch (Exception exception) {
+            log.error("Exception occurred", exception);
+            throw exception;
         }
     }
 }
