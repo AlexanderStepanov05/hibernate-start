d2eedd5e3dae20e8c083b912cf62d987a01725cb
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2024-03-04 18:26:03 +0300

    entity

--- src/main/java/org/example/HibernateRunner.java
+++ src/main/java/org/example/HibernateRunner.java
@@ -1,23 +1,39 @@
 package org.example;
 
+import org.example.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) throws SQLException {
 //        Connection connection = DriverManager.getConnection("db.url", "db.username", "db.password");
 
         Configuration configuration = new Configuration();
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+            session.beginTransaction();
+            User build = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 1))
+                    .age(20)
+                    .build();
+
+            session.persist(build);
+
+            session.getTransaction().commit();
         }
     }
 }
