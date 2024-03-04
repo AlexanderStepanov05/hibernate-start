c2ea57b8dc5421edebdd0047be7e67395fff04e2
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2024-03-04 17:38:39 +0300

    sessionFactory

---
+++ src/main/java/org/example/HibernateRunner.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class HibernateRunner {
+    public static void main(String[] args) throws SQLException {
+//        Connection connection = DriverManager.getConnection("db.url", "db.username", "db.password");
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            System.out.println("OK");
+        }
+    }
+}
